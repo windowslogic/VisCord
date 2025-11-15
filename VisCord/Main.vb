fbc274ff3483a63ac8ddf058c9648062bd2b86b8
Alethila <admin@windowslogic.co.uk>
2025-11-15 23:09:13 +0000

    1.4.0.0 For Windows 7 and 8.1.

--- VisCord/Main.vb
+++ VisCord/Main.vb
@@ -114,6 +114,34 @@ Public Class Main
             NSFWContentChecbox.Checked = False
         End If
 
+        'Load outbox items.
+        OutboxView.Items.Clear()
+
+        Dim savedData As String = My.Settings.OutboxList
+
+        If Not String.IsNullOrEmpty(savedData) Then
+            Dim OutboxItems As String() = savedData.Split(";"c)
+
+            For Each OutboxItem As String In OutboxItems
+                Dim OutboxParts As String() = OutboxItem.Split("|"c)
+
+                If OutboxParts.Length > 0 Then
+                    Dim listItem As New ListViewItem(OutboxParts(0))
+
+                    If OutboxParts.Length > 1 Then
+                        Dim subItems As String() = OutboxParts(1).Split(";"c)
+
+                        For Each subItem As String In subItems
+                            listItem.SubItems.Add(subItem.Trim())
+                        Next
+                    End If
+
+                    OutboxView.Items.Add(listItem)
+                End If
+            Next
+        Else
+        End If
+
         'Load correct icon.
         UpdateIcon()
 
@@ -163,7 +191,7 @@ Public Class Main
 
         'Load outbox data from external file.
         Try
-            My.Settings.OutboxList = File.ReadAllText(Application.StartupPath & "\Outbox.vco").ToString()
+            My.Settings.PinList1 = File.ReadAllText(Application.StartupPath & "\Outbox.vco").ToString()
         Catch
         End Try
 
@@ -198,30 +226,35 @@ Public Class Main
 
             My.Settings.HA = (Convert.ToInt32(TextBox1.Text))
 
-            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(14).ToString()
+            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(13).ToString()
+
+            My.Settings.Updates = (Convert.ToInt32(TextBox1.Text))
+
+            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(15).ToString()
 
             My.Settings.EnableNetwork = (Convert.ToInt32(TextBox1.Text))
 
-            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(16).ToString()
+            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(17).ToString()
 
             My.Settings.PinList1Name = TextBox1.Text
 
-            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(17).ToString()
+            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(18).ToString()
+
             My.Settings.PinList2Name = TextBox1.Text
 
-            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(18).ToString()
+            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(19).ToString()
 
             My.Settings.PinList3Name = TextBox1.Text
 
-            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(20).ToString()
+            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(21).ToString()
 
             My.Settings.Icon = (Convert.ToInt32(TextBox1.Text))
 
-            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(21).ToString()
+            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(22).ToString()
 
             My.Settings.NSFWFeatures = (Convert.ToInt32(TextBox1.Text))
 
-            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(22).ToString()
+            TextBox1.Text = File.ReadAllLines(Application.StartupPath & "\VisCord.ini").ElementAt(23).ToString()
 
             My.Settings.NSFWContent = (Convert.ToInt32(TextBox1.Text))
         Catch
@@ -970,8 +1003,9 @@ Public Class Main
             objWriter.WriteLine(My.Settings.Notify.ToString())
             objWriter.WriteLine("[Navigation]")
             objWriter.WriteLine(My.Settings.OpenExternal.ToString())
-            objWriter.WriteLine("[Performance / Cache]")
+            objWriter.WriteLine("[Performance / Cache / Updates]")
             objWriter.WriteLine(My.Settings.HA.ToString())
+            objWriter.WriteLine(My.Settings.Updates.ToString())
             objWriter.WriteLine("[Privacy]")
             objWriter.WriteLine(My.Settings.EnableNetwork.ToString())
             objWriter.WriteLine("[Pin List Names]")
